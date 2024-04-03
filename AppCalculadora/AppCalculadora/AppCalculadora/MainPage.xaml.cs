466068fd79c5d3250b57568654e6fdb3eb378d13
Stefany2 <1459565@senati.pe>
2024-04-03 15:55:25 -0500

    diseño calculadora

---
+++ AppCalculadora/AppCalculadora/AppCalculadora/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCalculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Btn_sumar(object sender, EventArgs e)
+        {
+            
+        }
+        private void Btn_restar(object sender, EventArgs e)
+        {
+            
+        }
+        private void Btn_por(object sender, EventArgs e)
+        {
+           
+        }
+        private void Btn_dividir(object sender, EventArgs e)
+        {
+           
+        }
+        private void Click_seven(object sender, EventArgs e)
+        {
+            
+        }
+        private void Click_eight(object sender, EventArgs e)
+        {
+            
+        }
+        private void Click_nine(object sender, EventArgs e)
+        {
+            
+        }
+        private void Btn_AC(object sender, EventArgs e)
+        {
+           
+        }
+        private void Click_four(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_five(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_six(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_C(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_one(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_two(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_three(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_equals(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_return(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_zero(object sender, EventArgs e)
+        {
+
+        }
+        private void Click_point(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
--- Usuarios/obj/Debug/net8.0/Usuarios.AssemblyInfo.cs
+++ Usuarios/obj/Debug/net8.0/Usuarios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44ee61e9cfa633542dd30d32a1f7b3ac099f287")]
 [assembly: System.Reflection.AssemblyProductAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Usuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
