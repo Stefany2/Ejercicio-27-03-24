a44ee61e9cfa633542dd30d32a1f7b3ac099f287
Stefany2 <1459565@senati.pe>
2024-04-03 10:53:58 -0500

    Agregar,Eliminar,Editar,Mostrar empleados

---
+++ Usuarios/Models/DatosContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Usuarios.Models;
+
+public partial class DatosContext : DbContext
+{
+    public DatosContext()
+    {
+    }
+
+    public DatosContext(DbContextOptions<DatosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server= (localDb)\\senati; database=Datos; integrated security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__empleado__FC29D6F64922E65C");
+
+            entity.ToTable("empleados");
+
+            entity.Property(e => e.IdEmpleado)
+                .ValueGeneratedNever()
+                .HasColumnName("ID_empleado");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Cargo)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("cargo");
+            entity.Property(e => e.CorreoElectronico)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("correo_electronico");
+            entity.Property(e => e.Departamento)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("departamento");
+            entity.Property(e => e.FechaContratacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("fecha_contratacion");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Salario)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("salario");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    internal class cs
+    {
+    }
+}
---
+++ Usuarios/Models/Empleado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Usuarios.Models;
+
+public partial class Empleado
+{
+    public int IdEmpleado { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Apellido { get; set; }
+
+    public string? CorreoElectronico { get; set; }
+
+    public string? Departamento { get; set; }
+
+    public string? Cargo { get; set; }
+
+    public DateTime? FechaContratacion { get; set; }
+
+    public decimal? Salario { get; set; }
+}
